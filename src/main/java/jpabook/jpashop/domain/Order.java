d995fb68cf3d77820ba9f0462a8570f126c04c39
JoJeHun <cho9759@gmail.com>
2023-12-31 01:21:46 +0900

    섹션 6. 주문 도메인 개발 - 주문 엔티티 개발 (생성 메서드 createOrder, 주문 취소 메서드 cancel, 전체 주문 가격 조회 getTotalPrice 내용 추가)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -47,4 +47,50 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    // 생성 메서드 createOrder : 회원정보, 배송정보, 주문상품정보로 주문 엔티티 생성
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // 비즈니스 로직
+    /*
+     주문 취소 로직 메서드 cancel : 주문 취소 시 사용
+     주문 상태를 취소로 변경 -> 주문 상품에 주문 취소 알리기
+     이미 배송 완료 시 -> 주문 취소 못하도록 예외처리
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회로직
+    // 전체 주문 가격 조회 : getTotalPrice()
+    public int getTotalPrice() {
+        int totalPrice = 0;
+
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
 }
