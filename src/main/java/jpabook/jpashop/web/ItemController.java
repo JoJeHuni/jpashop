0add669dfbf449cb6b5d20284e659859ce1ce1ae
JoJeHun <cho9759@gmail.com>
2024-01-03 01:29:38 +0900

    섹션 7. 웹 계층 개발 - 상품 수정 (컨트롤러 - updateItemForm 메서드, updateItem 메서드 추가, 수정 뷰 추가)

--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -47,4 +47,40 @@ public class ItemController {
 
         return "items/itemList";
     }
+
+    // 상품(Book) 수정 폼 updateItemForm 메서드 실행하여 조회 후 뷰로 전달
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    // 상품 수정 실행 폼 HTML 에는 id, 상품명, 가격, 수량 정보가 있음
+    // 정보 수정 후 submit -> updateItem() 실행 -> item 엔티티 인스턴스는 준영속 상태
+    // 영속성 컨텍스트의 지원 X, 데이터를 수정해도 변경 감지 기능 동작 X
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
 }
