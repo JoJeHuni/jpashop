3ad27375882ec2237c8433e004aeaf7d2bade8da
JoJeHun <cho9759@gmail.com>
2024-01-11 23:06:01 +0900

    활용 2 - 2. API 개발 고급 - 지연 로딩과 조회 성능 최적화 (V1 : 엔티티 직접 노출 - 컨트롤러 추가 (추천하지 않는 방식))

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * (Order - Member는 ManyToOne 관계다.)
+ * (Order - Delivery 는 OneToOne 관계다.)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        // return all; 이것을 바로 하면 문제가 생긴다. -> 왜?
+        // Order에 가보면 json이 Member를 뿌려야 한다. 그래서 Member로 가면 orders가 있다.
+        // 이것 때문에 끝나지 않고 반복하게 돼 장애가 생긴다.
+        // 양방향 관계 문제때문에 -> @JsonIgnore 가 필요하다. Member든 Order든. (Delivery, OrderItem도 Order와 관련이 있으니 필요하다.)
+
+        // 위의 @JsonIgnore 을 추가해도 오류가 해결되지 않는다. -> 왜?
+        // Order를 가지고 왔어도 fetch가 LAZY로 되어 있다. (지연 로딩이라는 뜻)
+        // 지연 로딩이면 member든 Delivery든 객체를 바로 가져오지 않는다.
+        // 로그를 읽어보면 bytebuddy 라는 것을 볼 수 있는데, 객체를 바로 가져오는 것이 아닌, Proxy 객체를 가져오기 때문이다.
+        // Json은 이 Proxy 객체를 json으로 어떻게 생성해야하는지 몰라서 예외가 발생하는 것이다.
+
+        // 해결 방법 : `Hibernate5Module` 을 스프링 빈으로 등록하면 해결
+
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+
+        return all;
+    }
+}
