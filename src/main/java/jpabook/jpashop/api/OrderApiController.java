1a5b146d5640f66cd1c6c3ae66af5ac1493510c5
JoJeHun <cho9759@gmail.com>
2024-01-17 01:23:42 +0900

    활용 2 - 3. API 개발 고급 - 컬렉션 조회 최적화 (V3 : 엔티티 -> Dto 변환 페치 조인 최적화) - 컨트롤러 V3코드, 리포지토리 findAllWithItem 추가

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -90,4 +90,17 @@ public class OrderApiController {
             count = orderItem.getCount();
         }
     }
+
+    /**
+     * 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
 }
